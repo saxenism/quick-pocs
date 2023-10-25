f6883488821d043d0f6840afe2ae8a1289cf00f8
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-10-25 10:09:12 +0530

    forge fmt

--- src/selfBalance.sol
+++ src/selfBalance.sol
@@ -2,29 +2,24 @@
 pragma solidity 0.8.21;
 
 contract AssemblyBalanceCheck {
-    function checkBalanceSolidity() public view returns(uint256 balance) {
+    function checkBalanceSolidity() public view returns (uint256 balance) {
         balance = address(this).balance;
     }
 
-    function checkBalanceAssemblySelfBalance() public view returns(uint256 contractBalance) {
+    function checkBalanceAssemblySelfBalance() public view returns (uint256 contractBalance) {
         assembly {
             contractBalance := selfbalance()
         }
     }
 
-    function checkBalanceAssemblyBalance() public view returns(uint256 contractBalance) {
+    function checkBalanceAssemblyBalance() public view returns (uint256 contractBalance) {
         assembly {
             contractBalance := balance(address())
         }
     }
 
-    function checkContractAddress() public view returns(bool isEqual) {
-        address solidityAddress = address(this);  
-
-
-
-
-        
+    function checkContractAddress() public view returns (bool isEqual) {
+        address solidityAddress = address(this);
         address assemblyAddress;
         assembly {
             assemblyAddress := address()
