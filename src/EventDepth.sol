a14c4e7b5ae2d6f5b41b1d6729b9d8f8cb328c1a
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-10-25 09:29:30 +0530

    initial commit: events depth

---
+++ src/EventDepth.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+///////
+/*
+This contract is used to test exactly how many arguments can we put in an event.
+From my research thus far, I can say that:
+1. With Solidity + non-anonymous events -> 3 indexed parameters + 12 non-indexed parameters + 1 **implicit** event signature parameter
+2. With Solidity + anonymous events -> 4 indexed parameters + 12 non-indexed parameters 
+
+So, all in all we can see that we can accomodate 16 parameters into the events fields, but according to the answer here: https://stackoverflow.com/a/49432998
+the number of arguments should have been 17. So, I don't really understand the mystery of this 1 missing parameter.
+
+Follow the discussion on X: https://x.com/saxenism/status/1716696062755565778?s=20
+*/
+///////
+
+contract EventDepth {
+    event Foo(uint8 indexed,uint8 indexed,uint8 indexed,uint8 indexed,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8) anonymous;
+    event Foo2(uint8 indexed,uint8 indexed,uint8 indexed,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8);
+
+    function emitEvent() public {
+        emit Foo(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+    }
+
+    function viewEventSelector() public pure returns(bytes32 eventSelector, bool isEqual) {
+        eventSelector = Foo2.selector;
+        isEqual = (eventSelector == keccak256("Foo2(uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8,uint8)"));
+    }
+}
